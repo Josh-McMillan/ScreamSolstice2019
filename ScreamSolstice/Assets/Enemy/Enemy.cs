1cfd2ca3ba80201432b7000955aefc0cbeffa1ec
Joshua McMillan <jmcmillan.programming@gmail.com>
2019-12-12 20:38:11 -0600

    Added Animations and AI Profiles

---
+++ ScreamSolstice/Assets/Enemy/Enemy.cs
@@ -0,0 +1,210 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public enum EnemyState
+{
+    RESPAWNING,
+    PURSUING,
+    STALKING,
+    HIDING,
+    CATCHING
+}
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private EnemyAIProfile profile;
+
+    [SerializeField] private LayerMask sightLayers;
+
+    [Header("DEBUGGING TOGGLES")]
+    [SerializeField] private bool disableFadeout = false;
+    [SerializeField] private bool disableRespawn = false;
+    [SerializeField] private bool disableHiding = false;
+
+    private EnemyState state = EnemyState.RESPAWNING;
+
+    private Transform playerTransform;
+    private Transform camTransform;
+
+    private NavMeshAgent myAgent;
+    private Animator myAnimator;
+    private Collider myCollider;
+
+    private bool hasHidden = false;
+    private bool hasStalked = false;
+
+    private void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        camTransform = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Transform>();
+        myAgent = GetComponent<NavMeshAgent>();
+        myAnimator = transform.GetChild(0).GetComponent<Animator>();
+        myCollider = GetComponent<Collider>();
+
+        myAgent.speed = profile.moveSpeed;
+    }
+
+    private void Update()
+    {
+        myAnimator.SetFloat("Blend", myAgent.velocity.magnitude / 20.0f);
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            state = EnemyState.RESPAWNING;
+        }
+
+        switch (state)
+        {
+            case EnemyState.RESPAWNING:
+                Respawn();
+                break;
+            case EnemyState.PURSUING:
+                ChasePlayer();
+                break;
+            case EnemyState.STALKING:
+                StalkPlayer();
+                break;
+            case EnemyState.HIDING:
+                HideFromPlayer();
+                break;
+            case EnemyState.CATCHING:
+                // Kill the Player
+                break;
+        }
+    }
+
+    private void Respawn()
+    {
+        myAgent.isStopped = true;
+
+        if (!disableRespawn)
+        {
+            Vector3 spawnPosition = GeneratePosition();
+
+            Collider[] hitColliders = Physics.OverlapSphere(spawnPosition + Vector3.up, 1.0f);
+
+            if (hitColliders.Length > 1)
+            {
+                Respawn();
+                return;
+            }
+            else
+            {
+                transform.position = spawnPosition;
+
+                if (IsInPlayerSight())
+                {
+                    Respawn();
+                    return;
+                }
+
+                switch (profile.followType)
+                {
+                    case FollowType.CHASE:
+                        state = EnemyState.PURSUING;
+                        break;
+
+                    case FollowType.STALK:
+                        state = EnemyState.STALKING;
+                        break;
+                }
+            }
+        }
+
+        myAnimator.SetBool("IsHidden", false);
+        myAgent.isStopped = false;
+        myCollider.enabled = true;
+    }
+
+    private void ChasePlayer()
+    {
+        Debug.Log("CHASING PLAYER!");
+
+        if (!myAgent.pathPending)
+        {
+            myAgent.SetDestination(playerTransform.position);
+        }
+
+        if (IsInPlayerSight() && !disableHiding)
+        {
+            state = EnemyState.HIDING;
+        }
+    }
+
+    private void StalkPlayer()
+    {
+        Debug.Log("STALKING PLAYER!");
+
+        if (Vector3.Distance(transform.position, playerTransform.position) <= profile.stalkingDistance)
+        {
+            myAgent.isStopped = true;
+        }
+        else if (!myAgent.pathPending)
+        {
+            myAgent.SetDestination(playerTransform.position);
+        }
+
+        if (IsInPlayerSight() && !disableHiding)
+        {
+            hasStalked = false;
+            state = EnemyState.HIDING;
+        }
+
+        hasStalked = true;
+    }
+
+    private void HideFromPlayer()
+    {
+        if (!hasHidden)
+        {
+            myAgent.isStopped = true;
+
+            if (!disableFadeout)
+            {
+                myAnimator.SetBool("IsHidden", true);
+            }
+
+            myCollider.enabled = false;
+            StartCoroutine(RespawnTimer());
+            hasHidden = true;
+        }
+    }
+
+    private Vector3 GeneratePosition()
+    {
+        float angle = Random.Range(0, Mathf.PI * 2.0f);
+
+        Vector2 position = new Vector2(Mathf.Sin(angle) * profile.spawnRadius, Mathf.Cos(angle) * profile.spawnRadius);
+
+        return new Vector3(playerTransform.position.x + position.x, 0.0f, playerTransform.position.z + position.y);
+    }
+
+    private bool IsInPlayerSight()
+    {
+        Vector3 direction = transform.position - camTransform.position;
+        float angle = Vector3.Angle(direction, camTransform.forward);
+
+        if (angle < profile.playerFieldOfView * 0.5f && !Physics.Linecast(transform.position + Vector3.up, playerTransform.position + Vector3.up, sightLayers))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    IEnumerator RespawnTimer()
+    {
+        float respawnTime = Random.Range(profile.minimumRespawnTime, profile.maximumRespawnTime);
+
+        Debug.Log("HALTING FOR " + respawnTime + " seconds!");
+
+        yield return new WaitForSeconds(respawnTime);
+
+        Debug.Log("RESPAWNING!");
+
+        state = EnemyState.RESPAWNING;
+        hasHidden = false;
+    }
+}
---
+++ ScreamSolstice/Assets/Enemy/EnemyAIProfile.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum FollowType
+{
+    CHASE,
+    STALK
+}
+
+[CreateAssetMenu(fileName = "AIProfile", menuName = "AI/Profile", order = 1)]
+public class EnemyAIProfile : ScriptableObject
+{
+    [Header("General")]
+
+    [Tooltip("How fast the enemy AI agent can go.")]
+    public float moveSpeed = 2.0f;
+
+    [Tooltip("The mode the AI is in. In CHASE, the AI will hunt down and kill the player. In STALK, the AI will follow but not approach the player.")]
+    public FollowType followType = FollowType.CHASE;
+
+    [Header("Respawning")]
+
+    [Tooltip("How far away the enemy spawns in a circle.")]
+    public float spawnRadius = 50.0f;
+
+    [Tooltip("Minimum amount of seconds it takes to respawn.")]
+    public float minimumRespawnTime = 8.0f;
+
+    [Tooltip("Maximum amount of seconds it takes to respawn.")]
+    public float maximumRespawnTime = 16.0f;
+
+
+    [Header("Stalking")]
+
+    [Tooltip("How far away in units the AI agent can approach player.")]
+    public float stalkingDistance = 25.0f;
+
+    [Header("Hiding")]
+
+    [Tooltip("At what angle from the camera the enemy can hide.")]
+    public float playerFieldOfView = 45.0f;
+}
